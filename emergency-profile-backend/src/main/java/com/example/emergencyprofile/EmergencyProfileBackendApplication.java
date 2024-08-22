8047fbfabcf7aaf14173213e007685bd20fa8907
Samir <bettahar.samir@outlook.com>
2024-08-22 11:51:54 +0100

    feat ✨(emergency profile): generated a spring boot application
    Signed-off-by: Samir <bettahar.samir@outlook.com>

---
+++ emergency-profile-backend/src/main/java/com/example/emergencyprofile/EmergencyProfileBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.emergencyprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyProfileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyProfileBackendApplication.class, args);
+	}
+
+}
---
+++ emergency-profile-backend/src/test/java/com/example/emergencyprofile/EmergencyProfileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emergencyprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyProfileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
